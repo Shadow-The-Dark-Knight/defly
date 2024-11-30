b656a65f47a2ade477d47557fbfde7225ef366cb
amir <nintendothing123@gmail.com>
2024-11-29 21:56:11 -0800

    refactored code still have to figure out how to do a page foor each individaul page

--- src/app/live/index.jsx
+++ src/app/live/index.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+"use client";
+
+import { React, useState } from "react";
 import { X, Settings } from "lucide-react";
 import Button from "@/components/mainButton";
+import Quality from "@/components/labels/quality";
 
-export default function live() {
+export default function Live() {
+  const [isOpen, setOpen] = useState(false);
+
+  const toggleSetting = () => {
+    setOpen((prev) => !prev);
+  };
+
   return (
     <div className=" flex justify-end h-scroll mt-3 ">
-      <div className="exit">
-        <X />
-      </div>
+      {/* ! is used to put compoent in previous state*/}
+      {!isOpen && (
+        <Settings className="text-white h-12 w-20" onClick={toggleSetting} />
+      )}
 
-      <div>
-        <Settings />
-      </div>
-
-      <div className="lg:w-4/12 md:w-8/12 bg-overlay rounded-md">
-        <Button label="Quality" href="/setting" />
-        <Button label="Controls" href="/control" />
-        <Button label="Chat" href="/chat" />
-        <Button label="Keybinds" href="keybinds" />
-        <Button label="Mechanics" href="/theme" />
-      </div>
-      <div></div>
+      {isOpen && (
+        <div className=" h-scroll mt-1.5  bg-overlay rounded-md lg:w-4/12 md:w-8/12 px-1 ">
+          <div className="flex justify-end">
+            <X
+              className=" text-white items-end cursor-pointer h-7 w-12"
+              onClick={toggleSetting}
+            />
+          </div>
+          <div>
+            <div className="flex">
+              <Button label="Quality" href="/setting" />
+              <Quality />
+            </div>
+            <div className="px-7 -mt-7">
+              <Button label="Controls" href="/control" />
+              <Button label="Chat" href="/chat" />
+              <Button label="Keybinds" href="keybinds" />
+              <Button label="Mechanics" href="/theme" />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
